2ad97beaeedb7719244598f9652267ac9db90be5
Sun Woo Kim <sun.kim101@outlook.com>
2025-07-03 02:04:58 +1000

    Blacklist nouveau

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ fi
 
 # pacman packages
 pacman_packages=(
+	linux-lts-headers
 	git
 	base-devel
 	hyprland
@@ -74,6 +75,70 @@ read -p "Install NVIDIA drivers? (y/n): " install_nvidia
 if [[ "$install_nvidia" == "y" || "$install_nvidia" == "Y" || "$install_nvidia" == "yes" || "$install_nvidia" == "YES" ]]; then
 	echo "[+] Installing NVIDIA drivers..."
 	sudo pacman -S --needed --noconfirm nvidia-lts nvidia-utils nvidia-settings
+	
+	# Create the suspend-hyprland.sh script
+	cat > /usr/local/bin/suspend-hyprland.sh << 'EOF'
+	#!/bin/bash
+
+	case "$1" in
+	    suspend)
+	        killall -STOP Hyprland
+	        ;;
+	    resume)
+	        killall -CONT Hyprland
+	        ;;
+	esac
+	EOF
+
+	# Make the script executable
+	chmod +x /usr/local/bin/suspend-hyprland.sh
+
+	# Create the hyprland-suspend.service systemd service file
+	cat > /etc/systemd/system/hyprland-suspend.service << 'EOF'
+	[Unit]
+	Description=Suspend hyprland
+	Before=systemd-suspend.service
+	Before=systemd-hibernate.service
+	Before=nvidia-suspend.service
+	Before=nvidia-hibernate.service
+
+	[Service]
+	Type=oneshot
+	ExecStart=/usr/local/bin/suspend-hyprland.sh suspend
+
+	[Install]
+	WantedBy=systemd-suspend.service
+	WantedBy=systemd-hibernate.service
+	EOF
+
+	# Create the hyprland-resume.service systemd service file
+	cat > /etc/systemd/system/hyprland-resume.service << 'EOF'
+	[Unit]
+	Description=Resume hyprland
+	After=systemd-suspend.service
+	After=systemd-hibernate.service
+	After=nvidia-resume.service
+
+	[Service]
+	Type=oneshot
+	ExecStart=/usr/local/bin/suspend-hyprland.sh resume
+
+	[Install]
+	WantedBy=systemd-suspend.service
+	WantedBy=systemd-hibernate.service
+	EOF
+
+	# Reload the systemd daemon and enable the newly created services
+	systemctl daemon-reload
+	systemctl enable hyprland-suspend
+	systemctl enable hyprland-resume
+
+	sudo tee /etc/modprobe.d/blacklist-nouveau.conf >/dev/null <<EOF
+	blacklist nouveau
+	options nouveau modeset-0
+	EOF
+
+	sudo mkinitcpio -P
 else
 	echo "[*] Skipping NVIDIA driver installation."
 fi
@@ -85,3 +150,4 @@ sudo systemctl enable sddm
 xdg-user-dirs-update
 
 echo "[!] Done"
+
