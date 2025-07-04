9992388ccc76cff164f58946bc947430bb676729
Sun Woo Kim <sun.kim101@outlook.com>
2025-07-05 06:08:56 +1000

    indentation fix

--- install.sh
+++ install.sh
@@ -106,55 +106,55 @@ if [[ "$install_nvidia" == "y" || "$install_nvidia" == "Y" || "$install_nvidia" 
 	
 	# Create the suspend-hyprland.sh script
 	cat > /usr/local/bin/suspend-hyprland.sh << 'EOF'
-	#!/bin/bash
+#!/bin/bash
 
-	case "$1" in
-	    suspend)
-	        killall -STOP Hyprland
-	        ;;
-	    resume)
-	        killall -CONT Hyprland
-	        ;;
-	esac
-	EOF
+case "$1" in
+	suspend)
+		killall -STOP Hyprland
+		;;
+	resume)
+		killall -CONT Hyprland
+		;;
+esac
+EOF
 
 	# Make the script executable
 	chmod +x /usr/local/bin/suspend-hyprland.sh
 
 	# Create the hyprland-suspend.service systemd service file
 	cat > /etc/systemd/system/hyprland-suspend.service << 'EOF'
-	[Unit]
-	Description=Suspend hyprland
-	Before=systemd-suspend.service
-	Before=systemd-hibernate.service
-	Before=nvidia-suspend.service
-	Before=nvidia-hibernate.service
+[Unit]
+Description=Suspend hyprland
+Before=systemd-suspend.service
+Before=systemd-hibernate.service
+Before=nvidia-suspend.service
+Before=nvidia-hibernate.service
 
-	[Service]
-	Type=oneshot
-	ExecStart=/usr/local/bin/suspend-hyprland.sh suspend
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/suspend-hyprland.sh suspend
 
-	[Install]
-	WantedBy=systemd-suspend.service
-	WantedBy=systemd-hibernate.service
-	EOF
+[Install]
+WantedBy=systemd-suspend.service
+WantedBy=systemd-hibernate.service
+EOF
 
 	# Create the hyprland-resume.service systemd service file
 	cat > /etc/systemd/system/hyprland-resume.service << 'EOF'
-	[Unit]
-	Description=Resume hyprland
-	After=systemd-suspend.service
-	After=systemd-hibernate.service
-	After=nvidia-resume.service
+[Unit]
+Description=Resume hyprland
+After=systemd-suspend.service
+After=systemd-hibernate.service
+After=nvidia-resume.service
 
-	[Service]
-	Type=oneshot
-	ExecStart=/usr/local/bin/suspend-hyprland.sh resume
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/suspend-hyprland.sh resume
 
-	[Install]
-	WantedBy=systemd-suspend.service
-	WantedBy=systemd-hibernate.service
-	EOF
+[Install]
+WantedBy=systemd-suspend.service
+WantedBy=systemd-hibernate.service
+EOF
 
 	# Reload the systemd daemon and enable the newly created services
 	systemctl daemon-reload
@@ -162,9 +162,9 @@ if [[ "$install_nvidia" == "y" || "$install_nvidia" == "Y" || "$install_nvidia" 
 	systemctl enable hyprland-resume
 
 	sudo tee /etc/modprobe.d/blacklist-nouveau.conf >/dev/null <<EOF
-	blacklist nouveau
-	options nouveau modeset=0
-	EOF
+blacklist nouveau
+options nouveau modeset=0
+EOF
 
 	sudo mkinitcpio -P
 else
