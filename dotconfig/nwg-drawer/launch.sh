44fb82d9225e4aedce344bcb0afdb49d90cafe52
Sun Woo Kim <sun.kim101@outlook.com>
2025-07-05 05:26:37 +1000

    fix nwg-drawer

--- dotconfig/nwg-drawer/launch.sh
+++ dotconfig/nwg-drawer/launch.sh
@@ -1,7 +1,7 @@
 style="glow.css"
 nwg-drawer -mb 8 -ml 8 -mr 8 -mt 8 -ovl -closebtn right -nocats -nofs -open \
--pblock '~/.config/hypr/scripts/power.sh lock' \
--pbpoweroff '~/.config/hypr/scripts/power.sh shutdown' \
--pbreboot '~/.config/hypr/scripts/power.sh reboot' \
--pbsleep '~/.config/hypr/scripts/power.sh suspend' \
+-pblock "sh $HOME/.config/hypr/scripts/power.sh lock" \
+-pbpoweroff "sh $HOME/.config/hypr/scripts/power.sh shutdown" \
+-pbreboot "sh $HOME/.config/hypr/scripts/power.sh reboot" \
+-pbsleep "sh $HOME/.config/hypr/scripts/power.sh suspend" \
 -s $style
